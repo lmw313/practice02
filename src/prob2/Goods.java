f99afbf18c82b1d331f4b5eff54fce614c03208f
notebook <lmw313@naver.com>
2016-07-08 15:26:58 +0900

    2nd commit

--- src/prob2/Goods.java
+++ src/prob2/Goods.java
@@ -2,4 +2,29 @@ package prob2;
 
 public class Goods {
 
+	public String name;
+	public String price;
+	public String countStock;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getCountStock() {
+		return countStock;
+	}
+	public void setCountStock(String countStock) {
+		this.countStock = countStock;
+	}
+
+	
+
 }
--- src/prob2/GoodsApp.java
+++ src/prob2/GoodsApp.java
@@ -8,12 +8,23 @@ public class GoodsApp {
 
 		Scanner scanner = new Scanner(System.in);
 		final int COUNT_GOODS = 3;
-		Goods[][] arrayGoods = new Goods[COUNT_GOODS][COUNT_GOODS];
+		Goods[] arrayGoods = new Goods[COUNT_GOODS];
+		
+		
 
 		for (int i = 0; i < COUNT_GOODS; i++) {
-				String info = scanner.nextLine();
-				String[] infos = info.split(" ");
-				System.out.println(infos[0] + "(가격:" + infos[1] + "원)이 " + infos[2] + "개 입고 되었습니다.");
+			Goods goods = new Goods();
+			String info = scanner.nextLine();
+			String[] infos = info.split(" ");
+			
+		    goods.setName(infos[0]);
+		    goods.setPrice(infos[1]);
+		    goods.setCountStock(infos[2]);
+		    arrayGoods[i] = goods;
+		}
+		
+		for(Goods a : arrayGoods){
+			System.out.println(a.getName() + "(가격:" + a.getPrice() + "원)이 " + a.getCountStock() + "개 입고 되었습니다.");
 		}
 		
 
